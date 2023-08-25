f781fa0289fac023b22e5a3ad11f5e221ca168ca
Jay Jagpal <jay@strangemother.com>
2023-08-25 23:36:12 +0100

    bundling

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,13 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    browser: './src/browser.js', // Browser entry point
-    module: './src/index.js'     // Node.js entry point (optional)
+    browser: './src/browser.js'    // Browser entry point
+    , module: './src/index.js'     // Node.js entry point (optional)
+    , dcss: './src/dcss.js'
+    , classgraph: './src/classgraph.js'
+    , bundle: [
+        'browser', 'dcss', 'classgraph'
+    ]
   },
   // output: {
   //   path: path.resolve(__dirname, 'dist'),
@@ -16,6 +21,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[fullhash:8].js',
     sourceMapFilename: '[name].[fullhash:8].map',
+    library: 'DynamicCSSStyleSheet', // Optional: set if you want to expose your package globally
     chunkFilename: '[id].[fullhash:8].js',
     libraryTarget: 'umd',         // Universal Module Definition
     clean: true
