7c8961716cc6b43408aa3436b5590cb65b0a3f50
Jay Jagpal <jay@strangemother.com>
2023-08-25 01:06:40 +0100

    webpack dynamic output file

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,17 @@ module.exports = {
     browser: './src/browser.js', // Browser entry point
     module: './src/index.js'     // Node.js entry point (optional)
   },
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   library: 'DynamicCSSStyleSheet', // Optional: set if you want to expose your package globally
+  //   filename: 'classy-declarative-css.js',
+  // },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    library: 'DynamicCSSStyleSheet', // Optional: set if you want to expose your package globally
-    filename: 'classy-declarative-css.js',
+    filename: '[name].[hash:8].js',
+    sourceMapFilename: '[name].[hash:8].map',
+    chunkFilename: '[id].[hash:8].js',
     libraryTarget: 'umd'         // Universal Module Definition
-  },
+  };
+
 };
