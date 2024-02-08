a07e684c641f97c5ad6bf0ab10446054f1a9c552
Jay Jagpal <jay@strangemother.com>
2024-02-08 01:22:55 +0000

    another go

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ export default {
   ],
   output: {
     file: 'dist/polyclass-browser.js', // Output file
-    format: 'iife', // Output format
+    // format: 'iife', // Output format
     name: 'polyclass', // Global variable name when included directly in the browser
     sourcemap: true // Optional: generates a source map
     // inlineDynamicImports: true
